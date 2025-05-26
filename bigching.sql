2b226cd45878ac62b81466d6858668709008cf22
kawausoJ <139613214+kawausoJ@users.noreply.github.com>
2025-05-26 23:13:10 +0800

    ORDER資料表新增備注

--- bigching.sql
+++ bigching.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-05-24 17:23:22
--- 伺服器版本： 10.4.28-MariaDB
--- PHP 版本： 8.2.4
+-- 產生時間： 2025-05-26 16:58:53
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -349,7 +349,8 @@ CREATE TABLE `order` (
   `Customer_ID` int(11) DEFAULT NULL COMMENT '顧客 ID (外鍵)',
   `Order_Date` datetime DEFAULT NULL COMMENT '訂購日期',
   `Order_exit` tinyint(4) DEFAULT NULL COMMENT '訂單是否已完成 (0=否, 1=是)',
-  `Pickup_Code` varchar(10) DEFAULT NULL COMMENT '取餐編號'
+  `Pickup_Code` varchar(10) DEFAULT NULL COMMENT '取餐編號',
+  `Remark` varchar(500) NOT NULL COMMENT '備注'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
